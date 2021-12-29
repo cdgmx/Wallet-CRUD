e9f4161a9db3aa6083b3b93869b042465fc2724b
cdgmx <iamcdgmx@gmail.com>
2021-12-30 05:40:04 +0800

    Update WalletController.ts
    Wallet CRUD controller functions

--- controllers/WalletController.ts
+++ controllers/WalletController.ts
@@ -1,40 +1,108 @@
-import asyncHandler from 'express-async-handler';
-import { Request, Response, NextFunction } from 'express';
-import  Wallet from '../models/WalletModel';
+import asyncHandler from "express-async-handler";
+import { Request, Response, NextFunction } from "express";
+import Wallet from "../models/WalletModel";
+import ApiError from "../error/ApiError";
 
-const createWallet = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    try{
-        const createdWallet = req.body.web3.eth.accounts.create()
-        const wallet = new Wallet({
-            balance: 0,
-            wallet_address: createdWallet.address
-        })
-        await wallet.save()
-        res.status(201).json({
-            success: true,
-            data: createdWallet
-        });
+const createWallet = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const createdWallet = req.body.web3.eth.accounts.create();
+      const wallet = new Wallet({
+        balance: 0,
+        wallet_address: createdWallet.address,
+      });
+      const savedWallet = await wallet.save();
+
+      if (!savedWallet) {
+        next(ApiError.internal("unable to save wallet"));
+      }
+
+      res.status(201).json({
+        data: createdWallet,
+      });
+    } catch (err: any) {
+      next(ApiError.internal("unable to create wallet"));
     }
-    catch(err){
-        next(err);
+  }
+);
+
+const readWallet = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { wallet_address } = req.body;
+      if (!wallet_address) {
+        next(ApiError.badRequest("wallet address is required"));
+      }
+      const wallet = await Wallet.findOne({
+        wallet_address: wallet_address,
+      });
+
+      if (!wallet) {
+        next(ApiError.notFound("wallet not found"));
+      }
+
+      res.status(200).json({
+        data: wallet,
+      });
+    } catch (err: any) {
+      next(ApiError.internal("unable to read wallet"));
     }
-});
+  }
+);
 
-const readWallet = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
-    console.log('readWallet');
-});
+const updateWallet = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { wallet_address, amount } = req.body;
+      if (!wallet_address) {
+        next(ApiError.badRequest("wallet address is required"));
+      }
+      const wallet = await Wallet.findOneAndUpdate(
+        {
+          wallet_address: req.params.wallet_address,
+        },
+        {
+          $inc: {
+            balance: amount,
+          },
+        }
+      );
 
-const updateWallet = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    console.log('updateWallet');
-});
+      if (!wallet) {
+        next(ApiError.notFound("wallet not found"));
+      }
 
-const deleteWallet = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    console.log('deleteWallet');
-});
+      res.status(200).json({
+        data: wallet,
+      });
+    } catch (err: any) {
+      next(ApiError.internal("unable to update wallet"));
+    }
+  }
+);
 
-export {
-    createWallet,
-    readWallet,
-    updateWallet,
-    deleteWallet
-}
+const deleteWallet = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { wallet_address } = req.body;
+      if (!wallet_address) {
+        next(ApiError.badRequest("wallet address is required"));
+      }
+      const wallet = await Wallet.findOneAndDelete({
+        wallet_address: wallet_address,
+      });
+
+      if (!wallet) {
+        next(ApiError.notFound("wallet not found"));
+      }
+
+      res.status(200).json({
+        data: wallet,
+      });
+    } catch (err: any) {
+      next(ApiError.internal("unable to delete wallet"));
+    }
+  }
+);
+
+export { createWallet, readWallet, updateWallet, deleteWallet };
