e1cde9cf0c053412c7af62b10816e29da3b786e7
cdgmx <iamcdgmx@gmail.com>
2021-12-30 01:37:36 +0800

    Update app.ts
    Added database connection

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import WalletRoutes from "./routes/WalletRoutes";
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
+dotenv.config();
+connectDB();
 
+app.use("/api/wallet", WalletRoutes);
 app.get("/", (req, res: Response) => {
   res.send("Hello World!");
 });
@@ -11,5 +16,3 @@ app.get("/", (req, res: Response) => {
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
-
