71d6bafbcd425a4e2224445aafa77a11b1280c48
cdgmx <iamcdgmx@gmail.com>
2021-12-30 03:04:59 +0800

    Update WalletController.ts
    create wallet functionality

--- controllers/WalletController.ts
+++ controllers/WalletController.ts
@@ -1,11 +1,26 @@
 import asyncHandler from 'express-async-handler';
 import { Request, Response, NextFunction } from 'express';
+import  Wallet from '../models/WalletModel';
 
 const createWallet = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-    console.log('createWallet');
+    try{
+        const createdWallet = req.body.web3.eth.accounts.create()
+        const wallet = new Wallet({
+            balance: 0,
+            wallet_address: createdWallet.address
+        })
+        await wallet.save()
+        res.status(201).json({
+            success: true,
+            data: createdWallet
+        });
+    }
+    catch(err){
+        next(err);
+    }
 });
 
-const readWallet = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+const readWallet = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
     console.log('readWallet');
 });
 
