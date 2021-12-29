cc8e0f73f46368a7b51e7b290561943f70287257
cdgmx <iamcdgmx@gmail.com>
2021-12-30 04:51:07 +0800

    Update app.ts
    Added errorhandler middleware

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import WalletRoutes from "./routes/WalletRoutes";
 import Web3Middleware from "./middlewares/Web3MiddleWare";
+import ApiErrorHandler from "./error/ApiErrorHandler";
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,7 +15,7 @@ app.use("/api/wallet", WalletRoutes);
 app.get("/", (req, res: Response) => {
   res.send("Hello World!");
 });
-
+app.use(ApiErrorHandler);
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${PORT}`);
 });
