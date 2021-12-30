8dd8164bbc7fd4847dd6ad4f16f91e17dcaa6aed
cdgmx <iamcdgmx@gmail.com>
2021-12-30 19:24:15 +0800

    Update AuthMiddleware.ts
    replaces req.body to req.query

--- middlewares/AuthMiddleware.ts
+++ middlewares/AuthMiddleware.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 import ApiError from "../error/ApiError";
 const walletVerification = (req:Request, res:Response, next:NextFunction) => {
     const web3 = req.body.web3;
-    web3.utils.isAddress(req.body.wallet_address) ? next() : next(ApiError.badRequest("Invalid Ethereum address"));
+    web3.utils.isAddress(req.query.wallet_address) ? next() : next(ApiError.badRequest("Invalid Ethereum address"));
 }
 
 export { walletVerification };
