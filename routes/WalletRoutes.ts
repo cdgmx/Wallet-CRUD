793d24f3d26e00df9d3dd35ef4f2844c5c165af5
cdgmx <iamcdgmx@gmail.com>
2021-12-30 10:47:40 +0800

    Update WalletRoutes.ts
    added walletverification middleware

--- routes/WalletRoutes.ts
+++ routes/WalletRoutes.ts
@@ -1,11 +1,11 @@
 import express, {Router} from 'express';
 import {createWallet, readWallet, updateWallet, deleteWallet} from '../controllers/WalletController';
-
+import {walletVerification} from '../middlewares/AuthMiddleware';
 const router:Router = express.Router();
 
 router.route('/create').post(createWallet);
-router.route('/read').get(readWallet);
-router.route('/update').put(updateWallet);
-router.route('/delete').delete(deleteWallet);
+router.route('/read').get(walletVerification,readWallet);
+router.route('/update').put(walletVerification,updateWallet);
+router.route('/delete').delete(walletVerification,deleteWallet);
 
 export default router;
