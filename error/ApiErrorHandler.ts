80a3a0217857874d99cfe184467bbbd06c478eac
cdgmx <iamcdgmx@gmail.com>
2021-12-30 10:46:36 +0800

    Update ApiErrorHandler.ts
    remove console log

--- error/ApiErrorHandler.ts
+++ error/ApiErrorHandler.ts
@@ -4,11 +4,13 @@ import ApiError from './ApiError';
 function apiErrorHandler(err:any, req: Request, res:Response, next:NextFunction) {
 
   if (err instanceof ApiError) {
+      console.log(err);
     res.status(err.code).json(err.message);
     return;
   }
 
   res.status(500).json('something went wrong');
+  return;
 }
 
 export default apiErrorHandler;
