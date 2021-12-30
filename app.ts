3e4451ca936551d52c9019b65c928bb422cfe250
cdgmx <iamcdgmx@gmail.com>
2021-12-31 03:28:11 +0800

    Update app.ts
    Build Frontend client

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import WalletRoutes from "./routes/WalletRoutes";
 import Web3Middleware from "./middlewares/Web3MiddleWare";
 import ApiErrorHandler from "./error/ApiErrorHandler";
+import path from "path";
 const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,15 @@ app.use("/api/wallet", WalletRoutes);
 app.get("/", (req:Request, res: Response) => {
   res.send("Hello World!");
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  });
+}
+
 app.use(ApiErrorHandler);
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on port ${PORT}`);
