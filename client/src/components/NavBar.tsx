277c7a7f0523f6fb1d84045daef17d40a5d54462
cdgmx <iamcdgmx@gmail.com>
2021-12-30 14:46:29 +0800

    Create NavBar.tsx
    Sample Navbar

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="container-fluid bg-warning">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="text-center text-white">
+                        Welcome to React
+                    </h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
