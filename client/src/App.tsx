c5fa202023d3abb353fcd8c89836e382a22a1384
cdgmx <iamcdgmx@gmail.com>
2021-12-30 12:59:00 +0800

    Added FrontEnd

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Container, Box } from '@mui/material'
+import React from 'react';
+
+function App() {
+  return (
+    <Container>
+      <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+    </Container>
+  );
+}
+
+export default App;
