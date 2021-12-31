223ff2dee6c5ca5c1a656a9daf8741cfc150f5ad
cdgmx <iamcdgmx@gmail.com>
2021-12-31 11:48:23 +0800

    Added Private Key Verification

--- middlewares/AuthMiddleware.ts
+++ middlewares/AuthMiddleware.ts
@@ -6,16 +6,33 @@ const walletVerification = (
   res: Response,
   next: NextFunction
 ) => {
-  const web3 = req.body.web3;
-  const { signedData, wallet_address }: any = req.query;
-  const publicAddress = web3.eth.accounts.recover(JSON.parse(signedData));
+  try {
+    const web3 = req.body.web3;
+    const { signedData, wallet_address, private_key }: any = req.query;
 
-  if (!web3.utils.isAddress(wallet_address)) {
-    next(ApiError.badRequest("Invalid Ethereum address"));
-  } else if (publicAddress !== wallet_address) {
-    next(ApiError.badRequest("You dont have access to this wallet"));
+    if (private_key) {
+      console.log("private_key");
+      const account = web3.eth.accounts.privateKeyToAccount(private_key);
+      console.log(account);
+      if (account.address !== wallet_address) {
+        next(ApiError.badRequest("Invalid wallet address"));
+      } else {
+        next();
+      }
+    } else {
+      const publicAddress = web3.eth.accounts.recover(JSON.parse(signedData));
+      console.log("next");
+      if (!web3.utils.isAddress(wallet_address)) {
+        next(ApiError.badRequest("Invalid Ethereum address"));
+      }
+      if (publicAddress !== wallet_address) {
+        next(ApiError.badRequest("You dont have access to this wallet"));
+      }
+      next();
+    }
+  } catch (err) {
+    next(ApiError.internal("Something Went Wrong"));
   }
-    next();
 };
 
 export { walletVerification };
